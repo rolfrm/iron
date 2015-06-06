22c9971d418b7b1e4184751c4e64bf9fd12fe1d5
Rolf Madsen <rolfrm@gmail.com>
2015-06-06 12:26:53 +0200

    added fail details to TEST_ASSERT

--- test.h
+++ test.h
@@ -3,7 +3,7 @@
 // testing methods should take no arguments and return a bool (pass/fail)
 extern int indent;
 #define TEST(fcn) {log("%*sTesting '" #fcn "'\n",++indent,""); if(fcn() == false){ERROR("%*sError during '" #fcn "'\n",indent--,""); return false;}else{log("%*sPass.\n",indent--,"");}}
-#define TEST_ASSERT(expr) if(false == (expr)){ERROR("Failed assertion"); return false;}
+#define TEST_ASSERT(expr) if(false == (expr)){ERROR("Failed assertion :'" #expr ";"); return false;}
 #define TEST_SUCCESS (true)
 #define TEST_FAIL (false)
 // test
