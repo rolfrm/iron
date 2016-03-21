bb85982c49e2da0f5b1373efa373118f5cc6a7b8
Rolf Madsen <rolfrm@gmail.com>
2016-03-21 09:44:22 +0100

    Added swap macro

--- test.h
+++ test.h
@@ -2,6 +2,7 @@
 // these macros are only to be used in testing methods
 // testing methods should take no arguments and return a bool (pass/fail)
 static int indent = 0;
+
 #define TEST(fcn) {log("%*sTesting '" #fcn "'\n",++indent,""); if(fcn() == false){ERROR("%*sError during '" #fcn "'\n",indent--,""); return false;}else{log("%*sPass.\n",indent--,"");}}
 #define TEST_ASSERT(expr) if(false == (expr)){ERROR("Failed assertion :'" #expr ";"); return false;}
 #define TEST_SUCCESS (true)
